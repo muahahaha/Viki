8a2466075a70459dcf03c34780ad22f2bf110c51
Oswald <savesomepaper@gmail.com>
2017-03-24 20:56:56 -0700

    Added get_parser() command_line_runner()

--- searchWikipedia.py
+++ searchWikipedia.py
@@ -1,4 +1,6 @@
 import wikipedia as wik
+import argparse
+
 
 def print_wiki_search(*args):
     try:
@@ -13,4 +15,30 @@ def pretty_print(*args):
     for i in range(len(args)):
         print(args[i])
 
-#TODO
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='search Wikipedia in command line'
+    )
+    parser.add_argument('query', metavar='QUERY',
+                        type=str,
+                        nargs='*',
+                        help='The words you want to search Wikipedia'
+                        )
+    return parser
+
+
+def command_line_runner():
+    parser = get_parser()
+    args = vars(parser.parse_args())
+
+    if not args['query']:
+        parser.print_help()
+        return
+
+    else:
+        print_wiki_search(args['query'])
+
+
+if __name__=='__main__':
+    command_line_runner()
